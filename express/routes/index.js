24d4d907fb004a7bb8c9db7073da1bbeb38cb821
penggang <918800642@qq.com>
2018-09-11 18:34:57 +0800

    添加公共头部

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 var http = require('http');
 
 /* GET home page. */
-router.post(/index/, function(req, res, next) {
+router.get(/index1/, function(req, res, next) {
   var Res = res
   var result = ''
   // Res.send(req.originalUrl);//req.originalUrl获取当前URL
-  Res.json({
-      hots:'123213'
-  })
-  // http.get('http://www.lizihang.com:80/landz-web-project/home/getHotRecommend?cityCode=110000&userId=BJG18052388719',function(res) {
-  //   res.setEncoding('utf-8'); //防止中文乱码
-  //   res.on('data',function (chunk) {
-  //     result += chunk
-  //   })
-  //   res.on('end', function() {
-  //     Res.render('index', { title: 'Express', result: JSON.parse(result)});
-  //   })
+  // Res.json({
+  //     hots:'123213'
   // })
+  http.get('http://www.lizihang.com:80/landz-web-project/home/getHotRecommend?cityCode=110000&userId=BJG18052388719',function(res) {
+    res.setEncoding('utf-8'); //防止中文乱码
+    res.on('data',function (chunk) {
+      result += chunk
+    })
+    res.on('end', function() {
+      Res.render('index', { title: 'Express', result: JSON.parse(result)});
+    })
+  })
   // Res.render('index', { title: 'Express' });
 });
 
